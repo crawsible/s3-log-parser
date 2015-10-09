7bab21c8cab40ae02fd08e5030a3a2b7afe819b7
Colin Jackson <colin.s.jackson90@gmail.com>
2015-10-09 02:31:10 -0400

    Get contents of latest logs

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
 	client := s3.New(&aws.Config{
 		Region: aws.String("us-west-2"),
 	})
+	downloader := s3manager.NewDownloader(&s3manager.DownloadOptions{
+		S3: client,
+	})
 
 	params := &s3.ListObjectsInput{
-		Bucket: aws.String("lattice-logs"),
-		Prefix: aws.String("logs/"),
+		Bucket:  aws.String("lattice-logs"),
+		MaxKeys: aws.Int64(1<<31 - 1),
+		Prefix:  aws.String("logs/"),
 	}
 
 	logFiles := []string{}
@@ -30,5 +35,19 @@ func main() {
 		return
 	}
 
-	fmt.Println(logFiles)
+	latestLogFiles := logFiles[len(logFiles)-21 : len(logFiles)-1]
+
+	buffer := &aws.WriteAtBuffer{}
+	for _, f := range latestLogFiles {
+		_, err := downloader.Download(buffer, &s3.GetObjectInput{
+			Bucket: aws.String("lattice-logs"),
+			Key:    aws.String(f),
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Println(string(buffer.Bytes()))
+	}
 }
